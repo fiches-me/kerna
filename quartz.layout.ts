98ae4d4738416fdb504007659522f3522143857c
Funasitien DreamCloud <86372093+Funasitien@users.noreply.github.com>
2024-09-19 16:14:12 +0200

    📱 Fix Responsive #2

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -27,13 +27,13 @@ export const defaultContentPageLayout: PageLayout = {
     Component.MobileOnly(Component.Spacer()),
     Component.Search(),
     Component.Darkmode(),
-    Component.MobileOnly(Component.Spacer()),
-    Component.Explorer(),
+    Component.DesktopOnly(Component.Explorer()),
   ],
   right: [
     //Component.Graph(),
     Component.TableOfContents(),
     Component.Backlinks(),
+    Component.MobileOnly(Component.Explorer()),
   ],
 }
 
